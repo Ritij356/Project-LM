9f97f674a71bcaac319e42f5380c7d49468c300f
Ritij Paudel <43158841+Ritij356@users.noreply.github.com>
2020-04-22 21:19:55 +0530

    Add files via upload

---
+++ signup.php
@@ -0,0 +1,92 @@
+<?php 
+session_start();
+error_reporting(0);
+if(isset($_POST['signup']))
+{
+
+if ($_POST["vercode"] != $_SESSION["vercode"] OR $_SESSION["vercode"]=='')  {
+        echo "<script>alert('Incorrect verification code');</script>" ;
+    } 
+        else { 
+$fname=$_POST['fullanme'];
+$mobileno=$_POST['mobileno'];
+$email=$_POST['email']; 
+$password=md5($_POST['password']); 
+$status=1;
+$sqlite="INSERT INTO  tblstudents(StudentId,FullName,MobileNumber,EmailId,Password,Status) VALUES(:StudentId,:fname,:mobileno,:email,:password,:status)";
+}
+}
+?>
+
+<!DOCTYPE html>
+<html >
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <link href="assets/css/bootstrap.css" rel="stylesheet" />
+   
+    
+</head>
+<body>
+
+<?php include('includes/header.php');?>
+
+
+                <h4 class="header-line">User Signup</h4>
+                
+                            </div>
+
+        </div>
+             <div class="row">
+           
+<div class="col-md-9 col-md-offset-1">
+               <div class="panel panel-danger">
+                        <div class="panel-heading">
+                           SINGUP FORM
+                        </div>
+                        <div class="panel-body">
+                            <form name="signup" method="post" onSubmit="return valid();">
+<div class="form-group">
+<label>Enter Full Name</label>
+<input class="form-control" type="text" name="fullanme" autocomplete="off" required />
+</div>
+
+
+<div class="form-group">
+<label>Mobile Number :</label>
+<input class="form-control" type="text" name="mobileno" maxlength="10" autocomplete="off" required />
+</div>
+                                        
+<div class="form-group">
+<label>Enter Email</label>
+<input class="form-control" type="email" name="email" id="emailid" onBlur="checkAvailability()"  autocomplete="off" required  />
+   <span id="user-availability-status" style="font-size:12px;"></span> 
+</div>
+
+<div class="form-group">
+<label>Enter Password</label>
+<input class="form-control" type="password" name="password" autocomplete="off" required  />
+</div>
+
+<div class="form-group">
+<label>Confirm Password </label>
+<input class="form-control"  type="password" name="confirmpassword" autocomplete="off" required  />
+</div>
+ <div class="form-group">
+<label>Verification code : </label>
+<input type="text"  name="vercode" maxlength="5" autocomplete="off" required style="width: 150px; height: 25px;" />&nbsp;<img src="captcha.php">
+</div>                                
+<button type="submit" name="signup" class="btn btn-danger" id="submit">Register Now </button>
+
+                                    </form>
+                            </div>
+                        </div>
+                            </div>
+        </div>
+    </div>
+    </div>
+
+</body>
+</html>
